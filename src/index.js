4524d83a4ca7dc65d693b6e7161c81f2d3b45ef4
Bartue <[bartbatiancila28@gmail.com]>
2024-12-01 10:36:27 +0800

    Import the styles file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import './styles/general.css';
+import './styles/leftContainer.css';
+import './styles/rightContainer.css';
 import { getWeatherForecast } from "./weatherData";
 
 getWeatherForecast('Polomolok')
