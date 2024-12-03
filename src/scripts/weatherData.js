a41868b6b91668b5cd74db26e4927d39be4448f4
Bartue <[bartbatiancila28@gmail.com]>
2024-12-03 19:48:50 +0800

    Add a unitGroup=metric to set all the data to metric. Import the weatherInfo js file and call the renderWeatherInfo to display/render into the page. Also refractor the error handling for better debugging

--- src/scripts/weatherData.js
+++ src/scripts/weatherData.js
@@ -1,8 +1,10 @@
 import { weatherForecast } from "./weatherForecast";
+import { weatherInfo } from "./weatherInfo";
 
 export async function getWeatherForecast(location, inputEl) {
+  const unitGroup = 'unitGroup=metric'
   const apiKey = 'EJYZJRLG2GL78ULSUWH2K4M8T';
-  let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apiKey}`
+  let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apiKey}&${unitGroup}`
 
   try {
     const response = await fetch(url);
@@ -10,16 +12,18 @@ export async function getWeatherForecast(location, inputEl) {
     if (!response.ok) {
 
       errorMessage(response, inputEl)
-
       throw new Error(`Response Status: ${response.status}`);
     }
 
     let data = await response.json();
 
     weatherForecast.renderCurrentCondition(data)
+    weatherInfo.renderWeatherInfo(data);
+
     console.log(data)
   } catch (error) {
-    console.log(error.message);
+    console.log('Fetch error: ', error);
+    throw error;
   }
 }
 
