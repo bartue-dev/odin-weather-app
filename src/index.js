d67664e7383e68e228e4dd0bbe3af824b5efad03
Bartue <[bartbatiancila28@gmail.com]>
2024-12-04 17:58:41 +0800

    Remove the imported weather pages

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import './styles/general.css';
 import './styles/leftContainer.css';
 import './styles/rightContainer.css';
 import { weatherForecast } from './scripts/weatherForecast';
+import { weatherPages } from './scripts/weatherPages';
 
 weatherForecast.searchLocation();
