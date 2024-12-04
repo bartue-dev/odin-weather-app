0a5fbf0b106430cf3d8a51cec5d7009cf38c28d3
Bartue <[bartbatiancila28@gmail.com]>
2024-12-04 17:59:38 +0800

    Formatted the file using the prettier formater

--- src/scripts/weatherData.js
+++ src/scripts/weatherData.js
@@ -1,39 +1,37 @@
-import { weatherForecast } from "./weatherForecast";
-import { weatherInfo } from "./weatherInfo";
+import { weatherForecast } from './weatherForecast';
+import { weatherInfo } from './weatherInfo';
 
 export async function getWeatherForecast(location, inputEl) {
-  const unitGroup = 'unitGroup=metric'
+  const unitGroup = 'unitGroup=metric';
   const apiKey = 'EJYZJRLG2GL78ULSUWH2K4M8T';
-  let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apiKey}&${unitGroup}`
+  let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apiKey}&${unitGroup}`;
 
   try {
     const response = await fetch(url);
-    
-    if (!response.ok) {
 
-      errorMessage(response, inputEl)
+    if (!response.ok) {
+      errorMessage(response, inputEl);
       throw new Error(`Response Status: ${response.status}`);
     }
 
     let data = await response.json();
 
-    weatherForecast.renderCurrentCondition(data)
+    weatherForecast.renderCurrentCondition(data);
     weatherInfo.renderWeatherInfo(data);
 
-    console.log(data)
+    console.log(data);
+    return data;
   } catch (error) {
     console.log('Fetch error: ', error);
-    throw error;
   }
 }
 
 function errorMessage(response, inputEl) {
-  let message = `${inputEl.value} not found`
+  let message = `${inputEl.value} not found`;
 
   if (response.status === 400) {
-    console.log(message)
+    console.log(message);
     inputEl.setCustomValidity(message);
     inputEl.reportValidity();
   }
 }
-
