7578ea9228dff0c32e4c6a0a57f711b4f1f70730
Bartue <[bartbatiancila28@gmail.com]>
2024-12-04 18:07:53 +0800

    Formatted the file using prettier formater

--- webpack.common.js
+++ webpack.common.js
@@ -18,17 +18,16 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.html$/i,
-        loader: "html-loader",
+        loader: 'html-loader',
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      }
-      
-    ]
-  }
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
