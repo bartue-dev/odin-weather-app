3d9c2b6e04bb42ec8fd8c23d13ba2176b32d119c
Bartue <[bartbatiancila28@gmail.com]>
2024-12-05 08:44:14 +0800

    Remove the searchLocation function from weatherForecast file. Add a loader feature. Loader display if the page is ready or not

--- src/index.js
+++ src/index.js
@@ -4,4 +4,18 @@ import './styles/rightContainer.css';
 import { weatherForecast } from './scripts/weatherForecast';
 import { weatherPages } from './scripts/weatherPages';
 
-weatherForecast.searchLocation();
+const loader = document.querySelector('.loader');
+const body = document.querySelector('body');
+
+document.onreadystatechange = () => {
+  if (document.readyState !== 'complete') {
+    body.style.visibility = 'hidden';
+    loader.style.visibility = 'visible';
+  }else {
+    loader.style.display = 'none';
+    body.style.visibility = 'visible';
+  }
+
+  console.log(document.readyState);
+  
+}
